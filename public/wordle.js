7d9d430fd9f28926e983817ef37cda0a9e85d7b5
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2022-05-15 23:32:28 +0000

    updates/changes

---
+++ public/wordle.js
@@ -0,0 +1,5 @@
+const keyboard = [
+  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Backspace"]
+];
