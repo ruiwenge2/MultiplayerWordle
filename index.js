484091b056cbe0b37919130357dcdf6886bf50e6
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2022-05-15 23:11:08 +0000

    yessss

--- index.js
+++ index.js
@@ -8,12 +8,29 @@ app.use(express.static("public"));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
+const waitingList = [];
+const rooms = {};
+
 global.guesses = [];
 global.answers = [];
+
 
 function random(num1, num2){
   return Math.round(Math.random() * (num2 - num1)) + num1;
 };
+
+function generateCode(){
+  const characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var code = "";
+  for(i = 0; i < 10; i++){
+    code += characters[random(0, characters.length - 1)];
+  }
+  return code;
+};
+
+function getWord(){
+  return answers[random(0, answers.length - 1)];
+}
 
 getGuesses().then(guesses => {
   global.guesses = guesses;
@@ -23,14 +40,27 @@ getAnswers().then(answers => {
   global.answers = answers;
 });
 
-
 io.on("connection", socket => {
-  socket.on("name", name => {
-    if(!name.replace(" ", "")){
-      socket.emit("name", `Player_${random(1000, 9999)}`);
+  socket.on("name", (name, type) => {
+    if(!name.replace(/\s/g, '')){
+      socket.emit("name", `Player_${random(1000, 9999)}`, type);
     } else {
-      socket.emit("name", true);
+      socket.emit("name", true, type);
     }
+  });
+
+  socket.on("waiting", name => {
+    waitingList.push(name);
+    console.log("ok");
+  });
+
+  socket.on("privateGame", () => {
+    let code = generateCode();
+    while(Object.keys(rooms).includes(code)){
+      code = generateCode();
+    }
+    rooms[code] = {};
+    socket.emit("gamecode", code);
   });
 });
 
@@ -43,11 +73,22 @@ app.get("/regular", (req, res) => {
 });
 
 app.get("/game/:id", (req, res) => {
-  res.render("multiplayer.html");
+  if(!Object.keys(rooms).includes(req.params.id)){
+    res.status(404).render("404.html");
+  } else {
+    res.render("multiplayer.html");
+  }
 });
+
+app.get("/getword", (req, res) => {
+  res.type("text/plain").send(getWord());
+});
+
+app.get("/*", (req, res) => {
+  res.status(404).render("404.html");
+})
 
 server.listen(3000, () => {
   console.log("server started");
 });
 
-// https://github.com/codergautam/wordle-wordlist
--- public/script.js
+++ public/script.js
@@ -1,5 +1,33 @@
+const socket = io();
+
 let name = localStorage.getItem("name");
+const input = document.getElementById("input");
 
 if(name){
-  document.getElementById("input").value = name;
+  input.value = name;
 }
+
+function playWithAnyone(){
+  socket.emit("name", input.value, "anyone");
+}
+
+function privateGame(){
+  socket.emit("name", input.value, "private");
+}
+
+
+socket.on("name", (res, type) => {
+  if(res != true){
+    localStorage.setItem("name", res);
+  }
+
+  if(type == "anyone"){
+    socket.emit("waiting", localStorage.getItem("name"));
+  } else {
+    socket.emit("privateGame");
+  }
+});
+
+socket.on("gamecode", code => {
+  location.href = `/game/${code}`;
+});
