d08f88aa0a7831bfd66718b22a5d5657a53b70a0
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2022-05-18 20:57:47 +0000

    pls make this work 🙏

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const path = require ('path');
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const { getGuesses, getAnswers } = require("./words");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname + "public")));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
